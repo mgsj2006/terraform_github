d57fa8e377a88b32dc47f3e62ea06e9c364f2195
mgsj2006 <mgsj2006@hotmail.com>
2025-10-22 12:06:42 +0100

    comment variables including test

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,26 +1,28 @@
-variable "subscription_id" {
-  type        = string
-  description = "The subscription ID for the Azure provider."
-  sensitive   = true
-}
+# variable "subscription_id" {
+#   type        = string
+#   description = "The subscription ID for the Azure provider."
+#   sensitive   = true
+# }
 
-variable "client_secret" {
+# variable "client_secret" {
 
-  type        = string
-  description = "The client secret for the Azure provider."
-  sensitive   = true
-}
+#   type        = string
+#   description = "The client secret for the Azure provider."
+#   sensitive   = true
+# }
 
-variable "tenant_id" {
+# variable "tenant_id" {
 
-  type        = string
-  description = "The tenant ID for the Azure provider."
-  sensitive   = true
-}
+#   type        = string
+#   description = "The tenant ID for the Azure provider."
+#   sensitive   = true
+# }
 
-variable "location" {
+# variable "location" {
 
-  type        = string
-  description = "The location for the Azure resource group."
-  default     = "East US"
-}
+#   type        = string
+#   description = "The location for the Azure resource group."
+#   default     = "East US"
+# }
+
+#test
